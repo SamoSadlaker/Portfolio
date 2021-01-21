f281372d082b3587d8622e2eaebb94540d98b0c1
SamoSadlaker <samkosadlak@gmail.com>
2021-01-21 17:39:08 +0100

    Responsible for mobile devices

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
   <meta property="og:title" content="SamoSadlaker's Portfolio" />
   <meta property="og:type" content="website" />
   <meta property="og:url" content="https://www.samosadlaker.eu/" />
-  <meta property="og:image" content="https://samosadlaker.eu/assets/img/test.jpg" />
+  <meta property="og:image" content="https://samosadlaker.eu/assets/img/logo.svg" />
   <meta property="og:description" content="My name is SamoSadlaker, I am a web developer. This is my portfolio where you can find information about me and my projects." />
   <meta property="og:site_name" content="Portfolio, SamoSadlaker" />
 
@@ -24,7 +24,18 @@
   <meta property="twitter:url" content="https://samosadlaker.eu/">
   <meta property="twitter:title" content="SamoSadlaker's Portfolio">
   <meta property="twitter:description" content="My name is SamoSadlaker, I am a web developer. This is my portfolio where you can find information about me and my projects.">
-  <meta property="twitter:image" content="">
+  <meta property="twitter:image" content="https://samosadlaker.eu/assets/img/logo.svg">
+
+  <!-- Favicons -->
+  <link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicons/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicons/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicons/favicon-16x16.png">
+  <link rel="manifest" href="assets/img/favicons/site.webmanifest">
+  <link rel="mask-icon" href="assets/img/favicons/safari-pinned-tab.svg" color="#008bf8">
+  <link rel="shortcut icon" href="assets/img/favicons/favicon.ico">
+  <meta name="msapplication-TileColor" content="#151515">
+  <meta name="msapplication-config" content="assets/img/favicons/browserconfig.xml">
+  <meta name="theme-color" content="#151515">
 
   <!-- Page Title -->
   <title>SamoSadlaker's Portfolio</title>
@@ -231,7 +242,7 @@
     </section>
 
 
-    <section id="Contact">
+    <!-- <section id="Contact">
       <div class="titles">
         <h3>Contact</h3>
         <span>contact me</span>
@@ -251,7 +262,7 @@
             <button type="submit" name="submit">Send message!</button>
           </form>
         </div>
-    </section>
+    </section> -->
 
   </main>
 
